a2a7d49594d6056dc7425e721d31364a179b59b8
Andrei Papko <andrei_papko@epam.com>
2016-04-06 15:06:05 +0300

    Revert "channels loading"
    This reverts commit d4d6993b32dad9c9d4b5072d72a4786968918588.

--- DB.js
+++
@@ -1,8 +0,0 @@
-var RNDBModel = require('react-native-db-models')
-
-var DB = {
-    "session": new RNDBModel.create_db('session'),
-    "channels": new RNDBModel.create_db('channels'),
-}
-
-module.exports = DB
